5c665883b1c1df65d3bb9e5a96a8ff1f67ba1e23
evanjkish <evanjkish@icloud.com>
2021-12-02 16:48:28 -0700

    implemented methods, getters, and setters, as well as private attributes

--- Item.java
+++ Item.java
@@ -1,4 +1,25 @@
 public class Item {
-    public String name;
-    public double price;
+    private String name;
+    private double price;
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    //getter
+    public String getName() {
+        return this.name;
+    }
+    public double getPrice() {
+        return this.price;
+    }
+
+    //setter 
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- Order.java
+++ Order.java
@@ -1,8 +1,71 @@
 import java.util.*;
 
 public class Order {
-    public String name;
-    public double total = 0;
-    public boolean ready;
-    public ArrayList<Item> items = new ArrayList<Item>();
+
+    private String name;
+    private boolean ready;
+    private ArrayList<Item> items = new ArrayList<Item>();
+
+    public Order() {
+        this.name = "Guest";
+        this.items = items;
+    }
+
+    public Order(String name) {
+        this.name = name;
+        this.items= items;
+    }
+
+    //getters
+    public String getName(){
+        return this.name;
+    }
+    public ArrayList<Item> getItems(){
+        return this.items;
+    }
+    public boolean getReady(){
+        return ready;
+    }
+
+    //setters
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setItems(ArrayList<Item> items){
+        this.items = items;
+    }
+    public void setReady(boolean ready){
+        this.ready = ready;
+    }
+
+    //methods
+    public void addItem(Item item){
+        this.items.add(item);
+    }
+
+    public String getStatusMessage(){
+        if (this.ready = true){
+            return "Your order is ready.";
+        } else {
+            return "Thank you for waiting. Your order will be ready soon.";
+        }
+    }
+
+    public double getOrderTotal(){
+        
+        double total = 0;
+        
+        for (Item i: this.items){
+            total += i.getPrice();
+        }
+        return total;
+    }
+    
+    public Order display(){
+        System.out.println("Name: " + this.name);
+        for(Item i: this.items){
+            System.out.println(i.getName() + " -$" + i.getPrice());
+        }
+        System.out.println("Total: $" + getOrderTotal());
+    }
 }
